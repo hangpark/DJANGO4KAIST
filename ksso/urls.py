92c151e353c8b9abef880bf1ad60e53fb7c667d0
Hang Park <hangpark@kaist.ac.kr>
2016-06-27 22:25:01 +0900

    delete urls

--- ksso/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import LoginView, LogoutView
-
-urlpatterns = patterns('',
-    url(r'^accounts/login/$', LoginView.as_view()),
-    url(r'^accounts/logout/$', LogoutView.as_view()),
-)
